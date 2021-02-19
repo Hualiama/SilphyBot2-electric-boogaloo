718b1981e45c4ec5205cb95824b175484ad9418f
Calum <calumgcoppack@gmail.com>
2021-02-18 23:59:24 -0500

    test fix for gate check

--- events.py
+++ events.py
@@ -9,8 +9,6 @@ async def gate_check(bot, message: discord.Message):
 	text = message.content
 	
 	if text.lower() == bot.gate_password.lower():
-		member_role = bot.get_guild(bot.server_id).get_role(bot.member_role_id)
-		
-		if member_role not in message.author.roles:
-			await message.author.add_roles(member_role)
-			slog.info("User {0} passed the gate!".format(message.author.display_name))
+		member_role = message.guild.get_role(bot.member_role_id)
+		await message.author.add_roles(member_role)
+		slog.info("User {0} passed the gate!".format(message.author.display_name))
