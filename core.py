b43b3e4ae69d0345253ede55b28883c83ad37576
Hualiama <69591288+Hualiama@users.noreply.github.com>
2024-08-24 16:33:20 -0500

    Update core.py

--- core.py
+++ core.py
@@ -2,8 +2,7 @@ from discord.ext import commands
 from botclasses import SilphyBot
 import slog
 
-
 class BaseCog(commands.Cog):
-	def __init__(self, bot: SilphyBot):
-		self.bot = bot
-		slog.log(f"Loaded cog {self.qualified_name}")
+    def __init__(self, bot: SilphyBot):
+        self.bot = bot
+        slog.log(f"Loaded cog {self.__class__.__name__}")
