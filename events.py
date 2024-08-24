f26ff932ad71e10335d306a3842e4e9668b91f49
Hualiama <69591288+Hualiama@users.noreply.github.com>
2024-08-24 16:33:29 -0500

    Update events.py

--- events.py
+++ events.py
@@ -1,14 +1,16 @@
 import slog
 import discord
 
+async def gate_check(bot: SilphyBot, message: discord.Message):
+    if not bot.gate_password:
+        return
 
-async def gate_check(bot, message: discord.Message):
-	if bot.gate_password == "":
-		return
-	
-	text = message.content
-	
-	if text.lower() == bot.gate_password.lower():
-		member_role = message.guild.get_role(bot.member_role_id)
-		await message.author.add_roles(member_role)
-		slog.info("User {0} passed the gate!".format(message.author.display_name))
+    if message.content.strip().lower() == bot.gate_password.lower():
+        member_role = message.guild.get_role(bot.member_role_id)
+        if member_role:
+            await message.author.add_roles(member_role)
+            slog.info(f"User {message.author.display_name} passed the gate!")
+        else:
+            slog.warning("Member role not found!")
+    else:
+        slog.warning(f"User {message.author.display_name} failed the gate check.")
