c2963c5c8d28cabc39bdd2a2eeedb94f0fdd1fa1
Hualiama <69591288+Hualiama@users.noreply.github.com>
2024-08-24 16:34:16 -0500

    Update silphy.py

--- silphy.py
+++ silphy.py
@@ -1,43 +1,49 @@
-# Silphy Bot v2.0.0a
-# By: Carbon
+# Silphy Bot2 v0.0.1a
+# By: Carbon and updated by Hualiama
 
-# TODO: Strike Member
-# TODO: Random Member
 from modules.gatemod import GateCommands
 from storage import Database
 from discord.ext import commands
-
 from modules.funmod import FunCommands
 from modules.adminmod import AdminCommands
 from modules.strikecommands import StrikeCommands
-
 from botclasses import SilphyBot
-
 import discord
 import logging
 import slog
 
 # Constants
-VERSION = "2.0.0a"
+VERSION = "0.0.1a"
 
-
-# client setup
+# Client setup
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True  # Ensure the bot can read message content
+
 silphy_bot = SilphyBot(intents=intents)
 
-	
 # Main program
 if __name__ == "__main__":
-	slog.log("Setting up logger...")
-	logging.basicConfig(level=logging.WARNING)
-	
-	slog.log("Loading settings...")
-	silphy_bot.load_settings()
-	
-	slog.log("Loading cogs...")
-	cogs = [AdminCommands(silphy_bot), FunCommands(silphy_bot), StrikeCommands(silphy_bot), GateCommands(silphy_bot)]
-	silphy_bot.load_cogs(cogs)
-	
-	slog.log("Starting Client...")
-	silphy_bot.run(silphy_bot.settings[SilphyBot.TOKEN_KEY])
+    slog.log("Setting up logger...")
+    logging.basicConfig(level=logging.INFO)  # Changed to INFO for more general-purpose logging
+    
+    slog.log("Loading settings...")
+    silphy_bot.load_settings()
+    
+    slog.log("Loading cogs...")
+    cogs = [
+        AdminCommands(silphy_bot),
+        FunCommands(silphy_bot),
+        StrikeCommands(silphy_bot),
+        GateCommands(silphy_bot)
+    ]
+    silphy_bot.load_cogs(cogs)
+
+    # Run the bot
+    token = silphy_bot.settings.get(SilphyBot.TOKEN_KEY)
+    if not token:
+        slog.error("No token found in settings file.")
+        sys.exit(1)
+        
+    slog.log("Starting bot...")
+    silphy_bot.run(token)
